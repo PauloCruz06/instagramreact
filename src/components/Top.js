f97e2508f7507e9d2858320e31ac6aa59033c8a4
Paulo <pc5783346@gmail.com>
2022-05-07 01:52:56 -0300

    Identação do componente Top

--- src/components/Top.js
+++ src/components/Top.js
@@ -4,28 +4,28 @@ import Image from "./Image";
 export default function Top() {
     return(
         <div class="top">
-        <div class="desktop">
-          <div class="logo">
-            <div><IonIcon name="logo-instagram" /></div>
-            <div class="border"></div>
-            <Image url="/images/logo.png" alt="Logo Instagram" />
-          </div>
-          <div class="browser">
-            <p>Pesquisar</p>
-          </div>
-          <div class="four-icons">
-            <IonIcon name="paper-plane-outline" />
-            <IonIcon name="compass-outline" />
-            <IonIcon name="heart-outline" />
-            <IonIcon name="person-outline" />
-          </div>
+            <div class="desktop">
+                <div class="logo">
+                    <div><IonIcon name="logo-instagram" /></div>
+                    <div class="border"></div>
+                    <Image url="/images/logo.png" alt="Logo Instagram" />
+                </div>
+                <div class="browser">
+                    <p>Pesquisar</p>
+                </div>
+                <div class="four-icons">
+                    <IonIcon name="paper-plane-outline" />
+                    <IonIcon name="compass-outline" />
+                    <IonIcon name="heart-outline" />
+                    <IonIcon name="person-outline" />
+                </div>
+            </div>
+            <div class="mobile">
+                <IonIcon name="logo-instagram" />
+                <Image url="/images/logo.png" alt="Logo Instagram" />
+                <IonIcon name="paper-plane-outline" />
+            </div>
         </div>
-        <div class="mobile">
-            <IonIcon name="logo-instagram" />
-            <Image url="/images/logo.png" alt="Logo Instagram" />
-            <IonIcon name="paper-plane-outline" />
-        </div>
-      </div>
     );
 }
 
